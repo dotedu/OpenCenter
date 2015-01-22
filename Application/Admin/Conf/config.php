2af91ff15eeadab608062571e300298764146a3c
xiaojuntao <xiaojuntao>
2015-01-22 16:13:16 +0800

    【修改】修改后台提示模板错误的bug

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -65,8 +65,8 @@ return array(
     'VAR_SESSION_ID' => 'session_id',    //修复uploadify插件无法传递session_id的bug
 
     /* 后台错误页面模板 */
-    'TMPL_ACTION_ERROR' => MODULE_PATH . 'View/Public/error.html', // 默认错误跳转对应的模板文件
-    'TMPL_ACTION_SUCCESS' => MODULE_PATH . 'View/Public/success.html', // 默认成功跳转对应的模板文件
-    'TMPL_EXCEPTION_FILE' => MODULE_PATH . 'View/Public/exception.html',// 异常页面的模板文件
+    'TMPL_ACTION_ERROR' => MODULE_PATH . 'View/default/Public/error.html', // 默认错误跳转对应的模板文件
+    'TMPL_ACTION_SUCCESS' => MODULE_PATH . 'View/default/Public/success.html', // 默认成功跳转对应的模板文件
+    'TMPL_EXCEPTION_FILE' => MODULE_PATH . 'View/default/Public/exception.html',// 异常页面的模板文件
     'DEFAULT_THEME' => 'default', // 默认模板主题名称
 );
