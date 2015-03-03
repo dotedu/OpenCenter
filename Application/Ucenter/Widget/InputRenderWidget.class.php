28c0b3928d3014ec81fde5b3856b6bf9e910e5f9
MingYang <xint5288@126.com>
2015-03-03 12:32:07 +0800

    改造用户扩展资料下拉菜单、多选默认值提交数据为键值、原有字符串格式不受影响 改造后当checkbox和select类型时，默认值可按照“1:男|2:女”这样的数组格式以及“字段名|表名”， 用户提交资料只提交数组键值和数据表ID字段值

--- Application/Ucenter/Widget/InputRenderWidget.class.php
+++ Application/Ucenter/Widget/InputRenderWidget.class.php
@@ -65,9 +65,11 @@ class InputRenderWidget extends Action
                 $this->assign('items', $items);
                 $this->assign('checked', $checked);
                 break;
+            
             /*
-             * 添加用户扩展资料可添加关联字段功能（多选支持）
-             * 给原有多选扩展字段做一个join判断
+             * 改造用户扩展资料下拉菜单、多选默认值提交数据为键值、原有字符串格式不受影响
+             * 改造后当checkbox和select类型时，默认值可按照“1:男|2:女”这样的数组格式以及“字段名|表名”，
+             * 用户提交资料只提交数组键值和数据表ID字段值
              * @MingYang <xint5288@126.com>
              */
             case 'checkbox':
@@ -76,6 +78,11 @@ class InputRenderWidget extends Action
                     if ($data['form_default_value'] != '' && $data['form_default_value'] != null) {
                         $canCheck = explode('|', $data['form_default_value']);
                         $items = null;
+                        /*
+                         * 添加用户扩展资料可添加关联字段功能（多选支持）
+                         * 给原有多选扩展字段做一个join判断
+                         * @MingYang <xint5288@126.com>
+                         */
                         //join判断,对关联扩展资料信息处理
                         if($data['child_form_type'] == "join"){
                             $joindata = explode('|',$data['form_default_value']);
@@ -91,29 +98,43 @@ class InputRenderWidget extends Action
                                 }
                             }
                             $this->assign('items', $items);
-                        //执行原扩展信息处理
                         } else {
-                        
-                            if (!$data['field_content']) {
-                                foreach ($canCheck as $key => $val) {
-                                    if ($val != '') {
-                                        $items[$key]['value'] = $val;
-                                        $items[$key]['selected'] = 0;
-                                    }
-                                }
-                            } else {
+                                /*
+                                 * 添加用户扩展资料可添加默认值添加默认值数组选项，用户直接提交键值而不是字符串、原有字符串提交功能不受影响（多选支持）
+                                 * @MingYang <xint5288@126.com>
+                                 */
+                                $vlaa = explode('|', $data['form_default_value']);
                                 $checked = explode('|', $data['field_content']['field_data']);
-                                foreach ($canCheck as $key => $val) {
-                                    if ($val != '') {
-                                        $items[$key]['value'] = $val;
-                                        if (in_array($val, $checked)) {
-                                            $items[$key]['selected'] = 1;
-                                        } else {
-                                            $items[$key]['selected'] = 0;
+                                foreach ($vlaa as $kye=>$vlaas){
+                                    $needle =':';
+                                    $tmparray = explode($needle,$vlaas);
+                                    if(count($tmparray)>1){
+                                        $vlab[] = explode(':', $vlaas); 
+                                        foreach ($vlab as $key=>$vlass){
+                                            $items[$key]['id'] = $vlass[0];
+                                            $items[$key]['value'] = $vlass[1];
+                                            $items[$key]['join'] = 1;
+                                            if (in_array($vlass[0], $checked)) {
+                                                $items[$key]['selected'] = 1;
+                                            } else {
+                                                $items[$key]['selected'] = 0;
+                                            }
+                                        }
+                                    } else {
+                                    //执行原扩展信息处理
+                                    $checked = explode('|', $data['field_content']['field_data']);
+                                        foreach ($canCheck as $key => $val) {
+                                            if ($val != '') {
+                                                $items[$key]['value'] = $val;
+                                                if (in_array($val, $checked)) {
+                                                    $items[$key]['selected'] = 1;
+                                                } else {
+                                                    $items[$key]['selected'] = 0;
+                                                }
+                                            }
                                         }
                                     }
                                 }
-                            }
                             $this->assign('items', $items);
                         }
                     }
@@ -123,15 +144,15 @@ class InputRenderWidget extends Action
                     $this->assign('checked', $checked);
                 }
                 break;
-            /*
-             * 添加用户扩展资料可添加关联字段功能（下拉菜单支持、相当于单选）
-             * 给原有下拉菜单做一个join判断
-             * @MingYang <xint5288@126.com>
-             */
             case 'select':
                 $this->assign('field_name', $data['field_name']);
                 $selected = isset($data['field_content']['field_data']) ? $data['field_content']['field_data'] : "还未选择";
                 if ($type == "personal") {
+                    /*
+                     * 添加用户扩展资料可添加关联字段功能（多选支持）
+                     * 给原有多选扩展字段做一个join判断
+                     * @MingYang <xint5288@126.com>
+                     */
                     //join判断,对关联扩展资料信息处理
                     if($data['child_form_type'] == "join"){
                         $joindata = explode('|',$data['form_default_value']);
@@ -141,17 +162,42 @@ class InputRenderWidget extends Action
                             $items[$key]['id'] = $val['id'];
                             $items[$key]['selected'] = ($selected == $val['id']) ? 'selected' : '';
                         }
-                    //执行原扩展信息处理   
+                    
                     } else {  
                         if ($data['form_default_value'] != '' && $data['form_default_value'] != null) {
+                            /*
+                             * 添加用户扩展资料可添加默认值添加默认值数组选项，用户直接提交键值而不是字符串、原有字符串提交功能不受影响（下拉支持）
+                             * @MingYang <xint5288@126.com>
+                             */
                             $canSelected = explode('|', $data['form_default_value']);
-                            $items = array();
-                            foreach ($canSelected as $key => $val) {
-                                $items[$key]['value'] = $val;
-                                $items[$key]['selected'] = ($selected == $val) ? 'selected' : '';
-                            }
-                            if (!isset($data['field_content']['field_data'])) {
-                                $items[0]['selected'] = 'selected';
+                            $checked = explode('|', $data['field_content']['field_data']);
+                            foreach ($canSelected as $kye=>$vlaas){
+                                $needle =':';
+                                $tmparray = explode($needle,$vlaas);
+                                if(count($tmparray)>1){
+                                    $vlab[] = explode(':', $vlaas);
+                                    foreach ($vlab as $key=>$vlass){
+                                        $items[$key]['id'] = $vlass[0];
+                                        $items[$key]['value'] = $vlass[1];
+                                        $items[$key]['join'] = 1;
+                                        if (in_array($vlass[0], $checked)) {
+                                            $items[$key]['selected'] = 'selected';
+                                        } else {
+                                            $items[$key]['selected'] = '';
+                                        }
+                                    }
+                                } else {
+                                    //执行原扩展信息处理
+                                    $items = array();
+                                    foreach ($canSelected as $key => $val) {
+                                        $items[$key]['value'] = $val;
+                                        $items[$key]['selected'] = ($selected == $val) ? 'selected' : '';
+                                    }
+                                    if (!isset($data['field_content']['field_data'])) {
+                                        $items[0]['selected'] = 'selected';
+                                    }
+                                }
+                            
                             }
                         } else {
                             $canSelected[0]['value'] = '无';
