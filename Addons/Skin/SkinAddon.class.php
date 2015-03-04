90180e429ebcd390ec475e2b947453a992e95f1e
郑钟良 <WebServiceSoftKing@163.com>
2015-03-04 10:53:37 +0800

    【修复】换肤安装卸载bug修复

--- Addons/Skin/SkinAddon.class.php
+++ Addons/Skin/SkinAddon.class.php
@@ -22,28 +22,28 @@ class SkinAddon extends Addon
         'author' => '想天科技-zzl(郑钟良)',
         'version' => '0.1'
     );
-
     public $custom_config = 'config.html';
 
     public function install()
     {
         $prefix = C("DB_PREFIX");
         $model = D();
-        $sql="REPLACE INTO `{$prefix}hooks` (`name`, `description`, `type`, `update_time`, `addons`) VALUES('setSkin', '设置个人皮肤', 2, 1425265259, 'Skin');";
+        $sql = "REPLACE INTO `{$prefix}hooks` (`name`, `description`, `type`, `update_time`, `addons`) VALUES('setSkin', '设置个人皮肤', 2, 1425265259, 'Skin');";
         $model->execute($sql);
-        $sql="DROP TABLE IF EXISTS `{$prefix}user_skin`;
-CREATE TABLE IF NOT EXISTS `{$prefix}user_skin` (
-  `uid` int(11) NOT NULL,
+        $model->execute("DROP TABLE IF EXISTS `{$prefix}user_skin`;");
+        $model->execute("CREATE TABLE IF NOT EXISTS `{$prefix}user_skin` (
+    `uid` int(11) NOT NULL,
   `config` varchar(200) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='用户皮肤配置';";
-        $model->execute($sql);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='用户皮肤配置';");
         return true;
     }
 
     public function uninstall()
     {
         $prefix = C("DB_PREFIX");
-        $sql="DELETE FROM `{$prefix}hooks` WHERE `name`='setSkin';DROP TABLE IF EXISTS `{$prefix}user_skin`";
+        $sql = "DROP TABLE IF EXISTS `{$prefix}user_skin`";
+        D('')->execute($sql);
+        $sql = "DELETE FROM `{$prefix}hooks` WHERE `name`='setSkin';";
         D('')->execute($sql);
         return true;
     }
@@ -53,7 +53,8 @@ CREATE TABLE IF NOT EXISTS `{$prefix}user_skin` (
      * @param $param 相关参数
      * @author 郑钟良<zzl@ourstu.com>
      */
-    public function setSkin($param){
+    public function setSkin($param)
+    {
         $this->display(T('Addons://Skin@Skin/skin'));
     }
 
@@ -65,15 +66,15 @@ CREATE TABLE IF NOT EXISTS `{$prefix}user_skin` (
      */
     public function pageHeader($param)
     {
-        $SkinsUrl=getRootUrl()."Addons/Skin/Skins/";
-        $config=getAddonConfig();
-        if($config['mandatory']==1){//强制执行管理员设置的默认皮肤
+        $SkinsUrl = getRootUrl() . "Addons/Skin/Skins/";
+        $config = getAddonConfig();
+        if ($config['mandatory'] == 1) { //强制执行管理员设置的默认皮肤
             // 载入换肤插件默认样式
-            echo '<link href="'.$SkinsUrl.$config['defaultSkin'].'/style.css" rel="stylesheet" type="text/css"/>';
-        }else{//执行用户设置样式
+            echo '<link href="' . $SkinsUrl . $config['defaultSkin'] . '/style.css" rel="stylesheet" type="text/css"/>';
+        } else { //执行用户设置样式
             // 载入换肤插件用户样式
-            $userSkin=getUserConfig();
-            echo '<link href="'.$SkinsUrl.$userSkin['skin'].'/style.css" rel="stylesheet" type="text/css"/>';
+            $userSkin = getUserConfig();
+            echo '<link href="' . $SkinsUrl . $userSkin['skin'] . '/style.css" rel="stylesheet" type="text/css"/>';
         }
     }
 }
