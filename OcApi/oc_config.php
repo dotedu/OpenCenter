31219c0f9e4c0e1bca4e8061686422c0344dc19b
xiaojuntao <xiaojuntao>
2015-03-05 20:34:46 +0800

    【修改】单点后台配置修改

--- OcApi/oc_config.php
+++ OcApi/oc_config.php
@@ -1,13 +1,2 @@
 <?php 
- return array(
-'SSO_SWITCH'=>'1', //单点登录开关
-'SSO_DB_HOST'=>'127.0.0.1', //用户中心主机
-'SSO_DB_NAME'=>'OpenCenter', //用户中心数据库名
-'SSO_DB_USER'=>'root', //用户中心数据库用户名
-'SSO_DB_PWD'=>'', //用户中心数据库密码
-'SSO_DB_PORT'=>'3306', //用户中心数据库端口
-'SSO_DB_PREFIX'=>'ocenter_', //用户中心数据库前缀
-'SSO_DATA_AUTH_KEY'=>'KOi7SehRQFmYVpAr8XngxyuBdq5GkETvMJbosZNc', //用户中心数据库密钥
-'OC_HOST'=>'http://localhost/opencenter', //Ocenter主机地址
-'APP_ID'=>'1', //应用ID
-);
+ return array()
