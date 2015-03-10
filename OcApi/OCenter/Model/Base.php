320463a78cd83b5971ebbb858193109878a22113
xiaojuntao <xiaojuntao>
2015-03-10 14:55:15 +0800

    【修改】单点登陆配置修改

--- OcApi/OCenter/Model/Base.php
+++ OcApi/OCenter/Model/Base.php
@@ -7,33 +7,49 @@
  * @author:xjw129xjt(肖骏涛) xjt@ourstu.com
  */
 
-require_once(OC_ROOT.'Lib/Mysql.php');
-require_once(OC_ROOT.'Lib/Think.php');
-class Base {
+require_once(OC_ROOT . 'Lib/Mysql.php');
+require_once(OC_ROOT . 'Lib/Think.php');
+class Base
+{
     var $_args = array();
     var $db;
-    var $AUTH_KEY ;
-    var $tablePre ;
+    var $AUTH_KEY;
+    var $tablePre;
     var $think;
     var $session_pre;
     var $appid;
-    function __construct() {
-        $db_config =  require(OC_ROOT. '../oc_config.php');
-        if(!$db_config['SSO_SWITCH']){
+
+    function __construct()
+    {
+        $db_config = require(OC_ROOT . '../oc_config.php');
+        if (!$db_config['SSO_SWITCH']) {
             return -1;
         }
 
-        $this->db = new Mysql($db_config['SSO_DB_NAME'],$db_config['SSO_DB_HOST'],$db_config['SSO_DB_USER'],$db_config['SSO_DB_PWD']);
-        $this->AUTH_KEY= $db_config['SSO_DATA_AUTH_KEY'];
+        $this->db = new Mysql($db_config['SSO_DB_NAME'], $db_config['SSO_DB_HOST'], $db_config['SSO_DB_USER'], $db_config['SSO_DB_PWD']);
+
+
+        $this->AUTH_KEY = $db_config['SSO_DATA_AUTH_KEY'];
         $this->tablePre = $db_config['SSO_DB_PREFIX'];
         $this->session_pre = $db_config['OC_SESSION_PRE'];
         $this->appid = $db_config['APP_ID'];
-        $this->think = new Think( $this->AUTH_KEY);
+        $this->think = new Think($this->AUTH_KEY);
+
+        $this->checkSwitch();
+
+    }
+
+    function checkSwitch()
+    {
+        $config = $this->db->getOne("SELECT value FROM `" . $this->tablePre . "config` WHERE name='_AUTHORIZE_SSO_SWITCH_USER_CENTER'");
+        if (!$config['value']) {
+            return '用户中心未开启单点登录';
+        }
     }
 
 
     /**
-     * check_username   验证用户名类型
+     * checkUsername   验证用户名类型
      * @param $username
      * @param $email
      * @param $mobile
@@ -41,7 +57,7 @@ class Base {
      * @return bool
      * @author:xjw129xjt(肖骏涛) xjt@ourstu.com
      */
-    function check_username(&$username, &$email, &$mobile, &$type = 0)
+    function checkUsername(&$username, &$email, &$mobile, &$type = 0)
     {
 
         if ($type) {
--- OcApi/oc_config.php
+++ OcApi/oc_config.php
@@ -1,6 +1,6 @@
 <?php 
  return array(
-'SSO_SWITCH'=>'0', //单点登录开关
+'SSO_SWITCH'=>'1', //单点登录开关
 'SSO_DB_HOST'=>'127.0.0.1', //用户中心主机
 'SSO_DB_NAME'=>'OpenCenter', //用户中心数据库名
 'SSO_DB_USER'=>'root', //用户中心数据库用户名
@@ -9,5 +9,6 @@
 'SSO_DB_PREFIX'=>'ocenter_', //用户中心数据库前缀
 'SSO_DATA_AUTH_KEY'=>'KOi7SehRQFmYVpAr8XngxyuBdq5GkETvMJbosZNc', //用户中心数据库密钥
 'OC_HOST'=>'http://localhost/opencenter', //Ocenter主机地址
+'OC_SESSION_PRE'=>'ocenter', //session前缀
 'APP_ID'=>'3', //应用ID
 );
