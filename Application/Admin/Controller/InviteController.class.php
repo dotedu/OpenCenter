a5e4c7ab4e48ef49b4f5b7a93cd2de7a8d59687d
郑钟良 <WebServiceSoftKing@163.com>
2015-03-23 17:20:57 +0800

    【启动】邀请注册

---
+++ Application/Admin/Controller/InviteController.class.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * 邀请注册
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 15-3-23
+ * Time: 下午2:52
+ * @author 郑钟良<zzl@ourstu.com>
+ */
+
+namespace Admin\Controller;
+
+
+use Admin\Builder\AdminListBuilder;
+
+class InviteController extends AdminController
+{
+
+    public function index()
+    {
+        $builder=new AdminListBuilder();
+        $builder->display();
+    }
+
+} 
